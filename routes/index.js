7917df1488bb134759a8b6932cadcb164a4c327c
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-07-30 18:03:48 +0530

    worked on beacon api

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@
 const { Router } = require('express');
 const organizationRoute = require('./organizationRoute');
 const shopRoute = require('./shopRoute');
+const beaconRoute = require('./beaconRoute');
 
 const routes = Router();
 routes.use(organizationRoute);
 routes.use(shopRoute);
+routes.use(beaconRoute);
 
 module.exports = routes;

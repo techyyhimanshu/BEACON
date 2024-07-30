09fbcaf13b8a59ca581fb8db0ffba5457e7eaeb8
techamdv <manudevarya690@gmail.com>
2024-07-31 02:13:01 +0530

    add some comment in template response

--- controllers/beaconController.js
+++ controllers/beaconController.js
@@ -1,137 +1,152 @@
-const db = require("../models")
-const Beacon = db.Beacon
-const Shop = db.Shop
+const db = require("../models");
+const Beacon = db.Beacon;
+const Shop = db.Shop;
 const Template = db.template;
-const TemplateType = db.temptype
+const TemplateType = db.temptype;
 
-
-const Sequelize = require("sequelize")
+const Sequelize = require("sequelize");
 const appclass = require("../appClass");
 
+// Function to add a new beacon
 const addBeacon = async (req, res) => {
     try {
+        // Retrieve the shop by its primary key (shop_id) from the request body
         const shop = await Shop.findByPk(req.body.shop_id);
-        if(!shop){
+        
+        // If the shop is not found, return a 404 error response
+        if (!shop) {
             return res.status(404).json({ status: "failure", message: "Shop not found" });
         }
-        const data = await Beacon.create(req.body)
+        
+        // Create a new beacon entry using the data from the request body
+        const data = await Beacon.create(req.body);
+        
+        // If the beacon is created successfully, return a success response
         if (data) {
-            res.status(200).json({ status: "success", message: "Created successfully" })
+            res.status(200).json({ status: "success", message: "Created successfully" });
         }
     } catch (error) {
+        // Handle Sequelize validation errors
         if (error instanceof Sequelize.ValidationError) {
             const errorMessages = error.errors.map(err => err.message);
             res.status(400).json({ status: "failure", message: errorMessages });
-        }else{
+        } else {
+            // Log any other errors and return a 500 internal server error response
             console.log(error);
+            res.status(500).json({ status: "failure", message: "Internal Server Error" });
         }
     }
+};
 
-}
-
-function getDifference(date_1,date_2) {
+// Function to calculate the difference in days between two dates
+function getDifference(date_1, date_2) {
     let date1 = new Date(date_1);
     let date2 = new Date(date_2);
 
-    // Calculating the time difference
-    // of two dates
-    let Difference_In_Time =
-        date2.getTime() - date1.getTime();
+    // Calculate the time difference between two dates in milliseconds
+    let differenceInTime = date2.getTime() - date1.getTime();
 
-    // Calculating the no. of days between
-    // two dates
-    let Difference_In_Days =
-        Math.round
-            (Difference_In_Time / (1000 * 3600 * 24));
-    console.log("day difference is " + Difference_In_Days)
-    return Difference_In_Days;
+    // Convert the time difference from milliseconds to days
+    let differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24));
+    
+    console.log("Day difference is " + differenceInDays);
+    return differenceInDays;
 }
 
-// search template for beacon 
+// Function to find the URL for a beacon based on its MAC address
 async function findUrl(macAddress) {
-    try{
-    var beaconData =await Beacon.findOne({
-        attributes : ["template_id"],
-        where:{
-            mac : macAddress
-            }
-        })
-        if (beaconData){
-            //check template existence
-            var templateData =await Template.findOne({
-                attributes : ['valid_from','valid_till','templateType_id','offer_data_1','offer_data_2'],
+    try {
+        // Find the beacon data by its MAC address
+        const beaconData = await Beacon.findOne({
+            attributes: ["template_id"],
+            where: { mac: macAddress }
+        });
+
+        // If beacon data is found, proceed to find the associated template
+        if (beaconData) {
+            const templateData = await Template.findOne({
+                attributes: ['valid_from', 'valid_till', 'templateType_id', 'offer_data_1', 'offer_data_2'],
                 where: {
                     template_id: beaconData.template_id,
-                    deletedAt : null
+                    deletedAt: null
                 }
-            })
-            console.log("template data is -------------" + templateData.get("valid_from"));
-            if(templateData != null){
-                today = new Date()
-                const beforeTime = getDifference(templateData.get("valid_from"),today)
-                const afterTime = getDifference(today,templateData.get("valid_till"))
+            });
 
-                // Is offer valid today
+            console.log("Template data is: " + templateData.get("valid_from"));
 
-                console.log("before time " + beforeTime + "----------- after time " + afterTime);
+            // If template data is found, check if the offer is valid today
+            if (templateData) {
+                let today = new Date();
+                const beforeTime = getDifference(templateData.get("valid_from"), today);
+                const afterTime = getDifference(today, templateData.get("valid_till"));
 
-                if(beforeTime >= 0 && afterTime > 0){
-                    console.log("offer is valid today");
-                    var templateTypeData =await TemplateType.findOne({
-                        attributes : ['template_path'],
-                        where:{
-                            templateType_id : templateData.templateType_id
-                        }
-                    })
-                    var lauchUrl = templateTypeData.template_path + templateData.offer_data_1 + '/' + templateData.offer_data_2 
-                    console.log("Successful ............. url is builded for beacon "+ lauchUrl);
-                    return lauchUrl;
-                }else{
-                    return "there is no offer valid today ....";
+                console.log("Before time: " + beforeTime + " - After time: " + afterTime);
+
+                // Check if the current date falls within the validity period of the offer
+                if (beforeTime >= 0 && afterTime > 0) {
+                    console.log("Offer is valid today");
+
+                    // Find the template type data to construct the URL
+                    const templateTypeData = await TemplateType.findOne({
+                        attributes: ['template_path'],
+                        where: { templateType_id: templateData.templateType_id }
+                    });
+
+                    // Construct the URL using the template path and offer data
+                    const launchUrl = templateTypeData.template_path + templateData.offer_data_1 + '/' + templateData.offer_data_2;
+                    console.log("URL built for beacon: " + launchUrl);
+                    return launchUrl;
+                } else {
+                    // Return a message if there is no valid offer today
+                    return "There is no offer valid today.";
                 }
             } else {
-                return "sorry !! template data is not fonud..........";
+                // Return a message if template data is not found
+                return "Sorry! Template data is not found.";
             }
         } else {
-            return "sorry !! beacon data is not fonud..........";
+            // Return a message if beacon data is not found
+            return "Sorry! Beacon data is not found.";
         }
-    } catch(e){
-        return "error occured : "+ e.message
-    } 
-    
+    } catch (e) {
+        // Return an error message if an exception occurs
+        return "Error occurred: " + e.message;
+    }
 }
 
+// Function to handle login and find the URL for a beacon
 const login = async (req, res) => {
     try {
+        // Find the beacon data by its MAC address
         const data = await Beacon.findOne({
-            where:{
-                mac:req.body.mac
-            }
-        })
+            where: { mac: req.body.mac }
+        });
+
+        // If beacon data is found, find the associated URL
         if (data) {
-            const builedUrl = await findUrl(req.body.mac)
-            //appclass.myclass();
-            //appclass.appclass();
-            if (builedUrl){
-                res.status(200).json({ status: "success", url:builedUrl })
-            } else {
-                res.status(200).json({ status: "success", url:"https://www.google.com" })
-            }
-        }
-        else{
-            res.status(200).json({ status: "beacon is not added yet", url:"https://www.google.com" })
+            const builtUrl = await findUrl(req.body.mac);
+
+            // Send the URL as part of the response, defaulting to Google if no URL is built
+            res.status(200).json({ status: "success", url: builtUrl || "https://www.google.com" });
+        } else {
+            // Return a message if the beacon is not added yet
+            res.status(200).json({ status: "beacon is not added yet", url: "https://www.google.com" });
         }
     } catch (error) {
+        // Handle Sequelize validation errors
         if (error instanceof Sequelize.ValidationError) {
             const errorMessages = error.errors.map(err => err.message);
             res.status(400).json({ status: "failure", message: errorMessages });
-        }else{
+        } else {
+            // Log any other errors and return a 500 internal server error response
             console.log(error);
+            res.status(500).json({ status: "failure", message: "Internal Server Error" });
         }
     }
+};
 
-}
-module.exports={
+// Exporting the functions to be used in other files
+module.exports = {
     addBeacon,
     login
-}
+};

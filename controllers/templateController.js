b9bd3a384cec2cc19da0e479de9bbe46145cf4b5
techamdv <manudevarya690@gmail.com>
2024-07-31 11:54:49 +0530

    template add to beacon

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -3,6 +3,8 @@ const Category = db.Category
 const TemplateType = db.temptype
 const Template = db.template
 const Shop = db.Shop
+const Beacon = db.Beacon
+
 
 
 const Sequelize = require("sequelize")
@@ -19,7 +21,21 @@ const createTemplate = async (req, res) => {
             offer_data_2:req.body.offer_data_2
         })
         if (data) {
-            res.status(200).json({ status: "success", message: "Created successfully" })
+
+            var addToBeacon = await Beacon.update({
+                template_id : data.template_id,
+                where : {
+                    // comment after token
+                    shop_id : req.body.shop_id
+                    // shop_id : req.shop_id ;
+                }
+            })
+            if (addToBeacon==0){
+                res.status(200).json({ status: "failure", message: "no beacon asign to this shop" })
+            }
+            else{
+                res.status(200).json({ status: "success", message: "Created successfully" })
+            }
         }
         else{
             res.status(200).json({ status: "fail", message: "data is not faount" ,data:data })
@@ -32,9 +48,9 @@ const createTemplate = async (req, res) => {
         }else{
             res.status(400).json({ status: "failure", message: error.message });
         }
-
     }
 }
+
 const getAllTemplate = async (req, res) => {
     try {
         const data = await Template.findAll({

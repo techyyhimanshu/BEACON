7fef631eb722d8a00c5c13b4599341ec60b38950
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-08-01 16:58:47 +0530

    changed token to authorization

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,24 +1,24 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebauthorization');
 
-const verifyToken = (req, res, next) => {
-  const { token } = req.headers;
-  if (!token) {
+const verifyauthorization = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
     return res.status(403).json({ status: "failure", message: "Unauthorized" });
   }
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(authorization, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       if (err.message === "jwt expired") {
-        return res.status(401).json({ status: "failure", message: "Token expired" });
+        return res.status(401).json({ status: "failure", message: "token expired" });
       }
       else {
         console.log(err.message);
-        return res.status(401).json({ status: "failure", message: "Failed to authenticate token" });
+        return res.status(401).json({ status: "failure", message: "Failed to authenticate authorization" });
       }
     }
-    // If token is valid, save decoded info to request for use in other routes
+    // If authorization is valid, save decoded info to request for use in other routes
     req.username = decoded.username;
     next();
   });
 };
 
-module.exports = verifyToken;
+module.exports = verifyauthorization;

94ac54ad3f4b2803670ae0b045c1f5f5dd16d5ee
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-08-08 10:51:19 +0530

    Added category add api

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -27,21 +27,6 @@ const addCategory=async(req,res)=>{
         
     }
 }
-
-// ADD CATEGORY 
-const addCategory =  async(req,res)=>{
-    try {
-        const category =await Category.create(req.body)
-        if(category){
-            res.status(200).json({status:"success", data:category})
-        }else{
-            res.status(404).json({status:"failure",message:"catory is not added"})
-        }
-    } catch (error) {
-        res.status(404).json({status:"failure",message:"Internal server error"})
-    }
-    }
-
 module.exports={
     getAllCategories,
     addCategory
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,5 +4,6 @@ const {getAllCategories, addCategory}=require("../controllers/categoryController
 // Define your routes here
 
 router.get("/api/categories",getAllCategories)
+router.post("/api/category",addCategory)
 
 module.exports = router;

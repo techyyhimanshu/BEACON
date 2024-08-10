f2071c5970e65f2c09785b801dce1a4d1ebc39e5
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-08-10 17:42:47 +0530

    created organzation menu and menu

---
+++ models/menu.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Menu extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Menu.init({
+    menu_id: {
+      type:DataTypes.INTEGER,
+      primaryKey:true
+    },
+    menu_name: {
+      type:DataTypes.STRING,
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'Menu',
+  });
+  return Menu;
+};
---
+++ models/orgmenu.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class OrgMenu extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  OrgMenu.init({
+    org_menu_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    menu_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+   org_id: {
+      type: DataTypes.INTEGER,
+      allowNull:false
+    },
+    alias: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+  }, {
+    sequelize,
+    modelName: 'OrgMenu',
+    tableName:"OrgMenu"
+  });
+  return OrgMenu;
+};

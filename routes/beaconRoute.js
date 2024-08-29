0197f70430140a4a3fa0266abd445e7b50252f74
techamdv <manudevarya690@gmail.com>
2024-08-29 17:44:26 +0530

    beacon fetch api updates

--- routes/beaconRoute.js
+++ routes/beaconRoute.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
-const {addBeacon, login, updateBeacon, getAllBeacons, getSingleBeacon,deleteBeacon, getBeaconsList }= require('../controllers/beaconController')
+const {addBeacon, updateBeacon, getAllBeacons, getSingleBeacon,deleteBeacon, getBeaconsList }= require('../controllers/beaconController')
 const verifyToken = require('../middlewares/authMiddleware');
 const router = Router();
 // Define your routes here
-router.get("/api/beacons",verifyToken,getAllBeacons)
+router.get("/api/beacons",getAllBeacons)
 router.get("/api/beacons/list",getBeaconsList)
 router.post("/api/beacon",verifyToken,addBeacon)
-router.post("/api/beacon/login",login)
+// router.post("/api/beacon/login",login)
 router.patch("/api/beacon/update",verifyToken,updateBeacon)
 router.get("/api/beacon/:id",verifyToken,getSingleBeacon)
 router.delete("/api/beacon/:id",deleteBeacon)

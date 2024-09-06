e9b8021cddbab3bc8efb9fc872a3f0a2b9d24914
techamdv <manudevarya690@gmail.com>
2024-09-06 16:21:57 +0530

    ADD GROUP BY

--- controllers/userController.js
+++ controllers/userController.js
@@ -415,10 +415,9 @@ const viewTime = async (req, res) => {
 const unregisteredUser = async (req, res) => {
     try {
         //const bodyData = req.body
-        const {count, rows} = await BeaconVisited.findAndCountAll({
-            attributes : ['device_id'],
-            distinct: true,
-            col: 'device_id'
+        const { count, rows } = await BeaconVisited.findAndCountAll({
+            attributes: ['device_id'],
+            group: ['device_id'], 
         });
         if (count > 0) {
             return res.status(200).json({ status: "success",count:count, data: rows })
